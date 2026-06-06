709ba9eca8f14051330103b37a7d6251c3bed41a
coboltaf-ops <coboltaf@gmail.com>
2026-06-05 22:21:19 -0500

    FUERZA REBUILD: Fondo blanco global debe aplicarse ahora

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Force rebuild: FONDO BLANCO GLOBAL */
 };
 
 export default nextConfig;
